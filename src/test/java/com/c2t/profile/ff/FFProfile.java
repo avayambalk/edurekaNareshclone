49dc88b0d7f7744e8643c79a0eed7fbdbd7f295a
connect2tech <message4naresh@gmail.com>
2018-03-11 12:39:19 +0530

    11.03.2018

---
+++ src/test/java/com/c2t/profile/ff/FFProfile.java
@@ -0,0 +1,37 @@
+package com.c2t.profile.ff;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.internal.ProfilesIni;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+
+public class FFProfile {
+
+	public static WebDriver driver;
+
+	private static String URL = "https://www.google.co.in/?gfe_rd=cr&dcr=0&ei=z2qiWoCZDaymX6mrt7AH&gws_rd=ssl";
+
+	@BeforeClass
+	public static void setUp() {
+		System.setProperty("webdriver.firefox.marionette", "geckodriver.exe");
+		
+		ProfilesIni ini = new ProfilesIni();
+		FirefoxProfile profile = ini.getProfile("SeleniumProfile");
+		
+		FirefoxOptions option=new FirefoxOptions();
+		option.setProfile(profile);
+		
+		
+		driver = new FirefoxDriver(option);
+		driver.manage().window().maximize();
+		driver.navigate().to(URL);
+	}
+
+	@Test
+	public void testWindowUsingName() {
+	}
+
+}
