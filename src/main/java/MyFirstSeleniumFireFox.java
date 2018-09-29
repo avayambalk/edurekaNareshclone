5d2180d2cbbdf4a23ff488abf6f73f8060b8c15a
connect2tech <message4naresh@gmail.com>
2018-09-29 22:14:04 +0530

    commit

---
+++ src/main/java/MyFirstSeleniumFireFox.java
@@ -0,0 +1,18 @@
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class MyFirstSeleniumFireFox {
+
+	public static void main(String[] args) {
+		// declaration and instantiation of objects/variables
+		System.setProperty("webdriver.firefox.marionette", "D:/nchaurasia/Automation-Architect/JavaSeleniumEdureka/driver/geckodriver.exe");
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.google.com/");
+		
+	}
+}
--- src/main/java/com/c2t/selenium/session1/MyFirstSelenium_HTML_Elements.java
+++ src/main/java/com/c2t/selenium/session1/MyFirstSelenium_HTML_Elements.java
@@ -11,20 +11,17 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class MyFirstSelenium_HTML_Elements {
 
 	public static void main(String[] args) {
-		String url = "file:///D:/nchaurasia/Automation-Architect/connect2tech.in-SeleniumWebDriver3.x_2/src/main/java/com/c2t/selenium/locate/LocatingMultipleElements.html";
+		String url = "file:///D:/nchaurasia/Automation-Architect/connect2tech.in-SeleniumWebDriver3.x_2/src/main/resources/LocatingMultipleElements.html";
 		// declaration and instantiation of objects/variables
 		System.setProperty("webdriver.firefox.marionette",
 				"D:/nchaurasia/Automation-Architect/connect2tech.in-SeleniumWebDriver3.x_2/driver/geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
 		driver.get(url);
-
-		List<WebElement> list = driver.findElements(By.name("gender"));
-
-		for (int i = 0; i < list.size(); i++) {
-			if (i == 2) {
-				list.get(i).click();
-			}
-		}
+		
+		WebElement we = driver.findElement(By.name("firstname"));
+		we.clear();
+		we.sendKeys("Welcome to selenium");
+		
 
 	}
 }
