fb8d282f4ac532117feca9b60cbe25a4f74ca7b0
connect2tech <message4naresh@gmail.com>
2018-12-02 12:02:10 +0530

    commit

--- src/main/java/com/c2t/ist1130/SingleDimensionalArray.java
+++ src/main/java/com/c2t/ist1130/SingleDimensionalArray.java
@@ -1,3 +1,4 @@
+package com.c2t.ist1130;
 
 /**
  * @author : Naresh Chaurasia
@@ -8,6 +9,14 @@
  * @YouTubeChannel: https://goo.gl/c7FAsq
  */
 
-public class Sample {
-
+public class SingleDimensionalArray {
+	public static void main(String[] args) {
+		int arr [] = new int [5];
+		
+		for(int i=0;i<arr.length ; i ++){
+			arr[i] = i + 10;
+			System.out.println(arr[i]);
+			
+		}
+	}
 }
