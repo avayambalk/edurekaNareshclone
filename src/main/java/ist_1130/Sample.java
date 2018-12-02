a9f0d0ff5d9c417dbc1d99376410548637d337d9
connect2tech <message4naresh@gmail.com>
2018-12-02 11:55:14 +0530

    commit

---
+++ src/main/java/ist_1130/Sample.java
@@ -0,0 +1,13 @@
+
+/**
+ * @author : Naresh Chaurasia
+ * @email : message4naresh@gmail.com
+ * @Code : https://bitbucket.org/connect2tech
+ * @WebSite : http://c2t.nchaurasia.in/ 
+ * @Profile : https://goo.gl/2mCt6v
+ * @YouTubeChannel: https://goo.gl/c7FAsq
+ */
+
+public class Sample {
+
+}
