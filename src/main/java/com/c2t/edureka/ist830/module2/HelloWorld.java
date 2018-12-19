b3830aa1b7c4baf4021ac96e38c93c854c80aa5b
connect2tech <message4naresh@gmail.com>
2018-12-19 20:47:46 +0530

    commit

---
+++ src/main/java/com/c2t/edureka/ist830/module2/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.c2t.edureka.ist830.module2;
+
+/**
+ * @author : Naresh Chaurasia
+ * @email : message4naresh@gmail.com
+ * @Code : https://bitbucket.org/connect2tech
+ * @WebSite : https://connect2tech.in/ 
+ * @Profile : https://goo.gl/2mCt6v
+ * @YouTubeChannel: https://goo.gl/c7FAsq
+ */
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world...");
+	}
+}
