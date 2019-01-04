f55870e36068605e16f6c105d5b64edab5bb6c5a
nc_global <message4naresh@gmail.com>
2019-01-04 12:11:37 +0530

    commit

---
+++ src/main/java/com/c2t/figmd/session2/LocatorId.java
@@ -0,0 +1,53 @@
+package com.c2t.figmd.session2;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * @author : Naresh Chaurasia
+ * @email : message4naresh@gmail.com
+ * @Code : https://bitbucket.org/connect2tech
+ * @WebSite : https://connect2tech.in/
+ * @Profile : https://goo.gl/2mCt6v
+ * @YouTubeChannel: https://goo.gl/c7FAsq
+ */
+
+public class LocatorId {
+	public static void main(String[] args) {
+		String url = "file:///D:/nchaurasia/Automation-Architect/connect2tech.in-SeleniumWebDriver3.x_2/src/main/resources/LocatingMultipleElements.html";
+		// declaration and instantiation of objects/variables
+		/*System.setProperty("webdriver.chrome.driver",
+				"D:/nchaurasia/Automation-Architect/connect2tech.in-SeleniumWebDriver3.x_2/driver/chromedriver.exe");*/
+		System.setProperty("webdriver.firefox.marionette", "geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.get(url);
+		
+		/*WebElement we1 = driver.findElement(By.id("fname"));
+		we1.clear();
+		we1.sendKeys("locating with id");*/
+		
+		/*WebElement we = driver.findElement(By.linkText("Emp100"));
+		we.click();*/
+		
+		/*WebElement we = driver.findElement(By.partialLinkText("Emp"));
+		we.click();*/
+		
+		/*WebElement we = driver.findElement(By.className("w3-input"));
+		we.clear();
+		we.sendKeys("This is last name");
+*/
+		
+		/*WebElement we = driver.findElement(By.tagName("input"));
+		we.clear();
+		we.sendKeys("using tags");*/
+		
+	
+		
+	}
+}
